da3bed77e56e6b3df3574c4a6048390a3cd0a426
zyletinee <geniiexw@gmail.com>
2025-06-23 18:51:24 +1000

    amending login system

--- Public/admin.py
+++ Public/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Meetings, Credentials
+from .models import Meetings
 
 admin.site.register(Meetings)
-admin.site.register(Credentials)
--- Public/models.py
+++ Public/models.py
@@ -1,17 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class Credentials(models.Model):
-    username = models.CharField(max_length=15)
-    password = models.CharField("password")
-    userID = models.IntegerField("ID", primary_key = True)
-    def __str__(self):
-        return self.username
-
-
 class Meetings(models.Model):
     date = models.DateTimeField("Date and Time")
     room = models.IntegerField("room")
-    userID = models.ForeignKey(Credentials, on_delete=models.CASCADE)
     def __str__(self):
         return str(self.date)
